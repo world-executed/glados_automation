a72a216886e0f52fad639b0653f5846b0a2504ab
world-executed <53502616+world-executed@users.noreply.github.com>
2024-01-22 10:56:21 +0800

    Update glados.py

--- glados.py
+++ glados.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
         if 'message' in checkin.text:
             mess = checkin.json()['message']
             print("签到成功") # 日志输出
-            # print(email+'----结果--'+mess+'----剩余('+time+')天')  # 日志输出
+            print(email+'----结果--'+mess+'----剩余('+time+')天')  # 日志输出
             sendContent += email+'----'+mess+'----剩余('+time+')天\n'
         else:
             requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'cookie已失效')
